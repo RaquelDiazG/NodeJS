a99f42c2e5796a17d68d0b7af7d8ebe71fa854cf
RaquelDiazG <r.diazgonz@alumnos.urjc.es>
2016-12-09 14:16:09 +0100

    Video 16 - Leer parámetros

--- proyecto/app.js
+++ proyecto/app.js
@@ -1,10 +1,14 @@
 var express=require("express");
+var bodyParser=require("body-parser");
 
 var app=express();
 
 //Servir archivos estaticos (imagenes,css,js)
 //la ruta sería esta: http://localhost:8080/app.css
 app.use(express.static("public")); 
+
+app.use(bodyParser.json()); //para peticiones application/json
+app.use(bodyParser.urlencoded({extended:true})); 
 
 app.set("view engine","jade");
 
@@ -14,6 +18,12 @@ app.get("/",function(req,res){
 
 app.get("/login",function(req,res){
 	res.render("login");
+});
+
+app.post("/users",function(req,res){
+	console.log("Email: "+ req.body.email);
+	console.log("Contraseña: "+ req.body.password);
+	res.send("Recibimos tus datos");
 });
 
 app.listen(8080);
